0a7fb61555d8127b5ad5cd9bfede75fbaa6e4f40
Jago-ral <j90769907@gmail.com>
2025-11-24 09:59:27 +0000

    fix swagger ui declaration

--- app/api-doc/page.tsx
+++ app/api-doc/page.tsx
@@ -3,7 +3,9 @@
 import dynamic from 'next/dynamic';
 import 'swagger-ui-react/swagger-ui.css';
 
-const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
+// const SwaggerUI = dynamic(() => import('swagger-ui-react'), { ssr: false });
+const SwaggerUI: any = dynamic(() => import('swagger-ui-react'), { ssr: false });
+
 
 export default function ApiDocPage() {
   return (
